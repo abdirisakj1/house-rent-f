acdbd23847836effc220b7794c52d2df900b6887
abdirisakj1 <abdirisakjaran@gmail.com>
2025-07-27 17:39:46 +0300

    Update BookingPage.jsx

--- src/pages/BookingPage.jsx
+++ src/pages/BookingPage.jsx
@@ -1,43 +1,46 @@
-import {useParams} from "react-router-dom";
 import {useEffect, useState} from "react";
 import axios from "axios";
-import AddressLink from "../AddressLink";
-import PlaceGallery from "../PlaceGallery";
+import PlaceImg from "../PlaceImg";
+import {differenceInCalendarDays, format} from "date-fns";
+import {Link} from "react-router-dom";
 import BookingDates from "../BookingDates";
 
-export default function BookingPage() {
-  const {id} = useParams();
-  const [booking,setBooking] = useState(null);
+export default function BookingsPage() {
+  const [bookings,setBookings] = useState([]);
   useEffect(() => {
-    if (id) {
-      axios.get('https://house-rent-bk.onrender.com/api/bookings').then(response => {
-        const foundBooking = response.data.find(({_id}) => _id === id);
-        if (foundBooking) {
-          setBooking(foundBooking);
-        }
-      });
-    }
-  }, [id]);
-
-  if (!booking) {
-    return '';
-  }
-
+    axios.get('https://house-rent-bk.onrender.com/api/bookings').then(response => {
+      setBookings(response.data);
+    });
+  }, []);
   return (
-    <div className="my-8">
-      <h1 className="text-3xl">{booking.place.title}</h1>
-      <AddressLink className="my-2 block">{booking.place.address}</AddressLink>
-      <div className="bg-gray-200 p-6 my-6 rounded-2xl flex items-center justify-between">
-        <div>
-          <h2 className="text-2xl mb-4">Your booking information:</h2>
-          <BookingDates booking={booking} />
-        </div>
-        <div className="bg-primary p-6 text-white rounded-2xl">
-          <div>Total price</div>
-          <div className="text-3xl">${booking.price}</div>
-        </div>
+    <div>
+      <div>
+        {bookings?.length > 0 ? (
+          bookings.map(booking => (
+            <Link to={`/account/bookings/${booking._id}`} className="flex gap-4 bg-gray-200 rounded-2xl overflow-hidden">
+              <div className="w-48">
+                <PlaceImg place={booking.place} />
+              </div>
+              <div className="py-3 pr-3 grow">
+                <h2 className="text-xl">{booking.place.title}</h2>
+                <div className="text-xl">
+                  <BookingDates booking={booking} className="mb-2 mt-4 text-gray-500" />
+                  <div className="flex gap-1">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-8 h-8">
+                      <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 8.25h19.5M2.25 9h19.5m-16.5 5.25h6m-6 2.25h3m-3.75 3h15a2.25 2.25 0 002.25-2.25V6.75A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25v10.5A2.25 2.25 0 004.5 19.5z" />
+                    </svg>
+                    <span className="text-2xl">
+                      Total price: ${booking.price}
+                    </span>
+                  </div>
+                </div>
+              </div>
+            </Link>
+          ))
+        ) : (
+          <div className="text-center text-gray-500 py-8">No bookings found.</div>
+        )}
       </div>
-      <PlaceGallery place={booking.place} />
     </div>
   );
 }
